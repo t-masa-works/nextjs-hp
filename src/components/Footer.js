1cef54c3004700a89b3b8e07ea3454b8d06da815
T.M <t.masa.works @gmail.com>
2025-08-25 14:59:54 +0900

    余白修正

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
 
-      <div className="bg-green-500 text-center">
+      <div className="bg-green-500 h-16 flex items-center justify-center">
         <small>&copy;T.Masa.Works 2025</small>
       </div>
   );
